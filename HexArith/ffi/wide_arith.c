230520e0ea075a6150d04e2a154870ff9d6c7972
Kim Morrison <477956+kim-em@users.noreply.github.com>
2026-04-28 22:11:19 +1000

    feat: add UInt64.mulFull helper (#736)
    Progress: progress/2026-04-28T11:54:09Z_00f9679d.md

--- HexArith/ffi/wide_arith.c
+++ HexArith/ffi/wide_arith.c
@@ -5,6 +5,14 @@ LEAN_EXPORT uint64_t lean_hex_uint64_mul_hi(uint64_t a, uint64_t b) {
     return (uint64_t)(((__uint128_t)a * (__uint128_t)b) >> 64);
 }
 
+LEAN_EXPORT lean_obj_res lean_hex_uint64_mul_full(uint64_t a, uint64_t b) {
+    __uint128_t p = (__uint128_t)a * (__uint128_t)b;
+    lean_object* pair = lean_alloc_ctor(0, 2, 0);
+    lean_ctor_set(pair, 0, lean_box_uint64((uint64_t)(p >> 64)));
+    lean_ctor_set(pair, 1, lean_box_uint64((uint64_t)p));
+    return pair;
+}
+
 LEAN_EXPORT lean_obj_res lean_hex_uint64_add_carry(uint64_t a, uint64_t b, uint8_t cin) {
     uint64_t sum;
     uint64_t total;
